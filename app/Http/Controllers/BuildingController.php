07c1323e8bc6f3e15cd302427e5e92e68c55c98a
ConnorLinacre <U1653940@unimail.hud.ac.uk>
2021-04-30 13:04:39 +0100

    Added controller for buildings

--- app/Http/Controllers/BuildingController.php
+++ app/Http/Controllers/BuildingController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Building;
 use Illuminate\Http\Request;
 
 class BuildingController extends Controller
 {
-    //
+    public function index() {
+        return view('content.building.table', ['building' => Building::all(),]);
+    }
+
+    public function create() {
+        return view('content.building.create');
+    }
+
+    public function store(Request $request) {
+        $building = Building::create([
+            'name' => $request->input('name'),
+            'address' => $request->input('address')
+        ]);
+        $building->save();
+    }
+
+    public function view(Building $building) {
+        return view('content.building.view', ['building', $building]);
+    }
+
+    public function edit(Building $building) {
+        return view('content.building.update', ['building', $building]);
+    }
+
+    public function update(Request $request, Building $building) {
+        $building->update([
+            'name' => $request->input('name'),
+            'address' => $request->input('address')
+        ]);
+        $building->save();
+    }
+
+    public function destroy(Building $building) {
+        $building->delete();
+        return redirect()->route('all_buildings');
+    }
 }
