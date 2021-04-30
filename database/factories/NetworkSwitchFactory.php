387461380b8ab7a52b8a0ff5bb3c4c1a2af48542
ConnorLinacre <U1653940@unimail.hud.ac.uk>
2021-04-30 13:12:56 +0100

    Added skeleton files for network switches

---
+++ database/factories/NetworkSwitchFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\NetworkSwitch;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class NetworkSwitchFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = NetworkSwitch::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
