562e7aa2750e2e36afd195ed1ab10c22914f288f
ConnorLinacre <U1653940@unimail.hud.ac.uk>
2021-04-30 12:35:13 +0100

    Added campus routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +15,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/campus/create', 'CampusController@create')->name('create_campus');
+    Route::post('/campus/create', 'CampusController@store')->name('create_campus');
+    Route::get('/campus/update/{campus}', 'CampusController@edit')->name('edit_campus');
+    Route::post('/campus/update/{campus}', 'CampusController@update')->name('edit_campus');
+    Route::get('/campus/delete/{campus}', 'CampusController@destroy')->name('delete_campus');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
 Auth::routes();
 
-Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/campus', 'CampusController@index')->name('all_campus');
+Route::get('/campus/{campus}', 'CampusController@show')->name('view_campus');
 
